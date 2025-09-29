ed16f2fd15d81d95d3b634b5ec8da4e25c03c090
amorama0112 <amorama0112@iescarrillo.es>
2025-09-29 08:47:44 +0200

    Añadir ejemplos de la sintaxis basica de typescript, modificacion del package.json y gitignore

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,56 @@
-let x = 1
-console.log("Hola2");
+/***    
+ * 
+ * Tipos de datos primitivos----------------------
+ * string
+ * number
+ * boolean
+ * 
+ */
+
+let nombre:string = "Jose";
+let edad:number = 18;
+let mayorEdad:boolean = true;
+
+
+/***    
+ * 
+ * Tipos de datos especiales----------------------
+ * any
+ * unknown
+ * void
+ * 
+ */
+
+let cualquierCosa:any = "hola";
+cualquierCosa = 10;
+cualquierCosa = true;
+
+let tipoDesconocido:unknown = "hola";
+tipoDesconocido = 10;
+tipoDesconocido = "adios";
+
+if(typeof tipoDesconocido === "string"){
+    console.log(tipoDesconocido.toUpperCase());
+}
+console.log("Tu nombre es " + nombre);
+console.log(`Tu nombre es ${nombre}`);
+/***
+ * 
+ * Declaracion de variables-----------------------
+ * var (declaracion global)
+ * let (declaracion de bloque)
+ * const (declaracion de constantes)
+ */
+
+
+var x;
+x="Jose";
+
+if(true){
+    x="maria";
+}
+
+console.log(x);
+
+let y;
+const z = 10;
